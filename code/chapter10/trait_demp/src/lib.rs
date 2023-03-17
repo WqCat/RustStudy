b93f95b1d1755aa8e888d5ba5af3cda389fca6a9
WqCat <2416603799@qq.com>
2023-03-17 10:03:37 +0800

    master

--- code/chapter10/trait_demp/src/lib.rs
+++ code/chapter10/trait_demp/src/lib.rs
@@ -6,6 +6,44 @@ pub trait Summary {
     }
 }
 
+use std::fmt::Display;
+//新增
+pub fn notify01(item: impl Summary + Display) {   //impl Summary 表明实现了Summary的某个函数，    有其他类型是实现了Summary，那个类型可以使用该方法
+    println!("ASFLKN:{}",item.summarize())
+}
+
+pub fn notify02<T: Summary + Display>(item1: T,item2: T) {   //同上，更简单
+    println!("ASFLKN:{}",item1.summarize())
+}
+
+use std::fmt::Debug;
+pub fn notify03<T,U>(a: T,b: U) -> String   //where,看起来更好看
+where 
+    T: Summary +Display,
+    U: Clone +Debug,
+{   
+    println!("ASFLKN:{}",a.summarize())
+}
+
+//实现trait作为返回类型 ,其返回的类型必须是同一个，不能返回两种可能的类型,例如：if/else两种情况
+pub fn notify04(s: &str) -> impl Summary {   //同上，更简单
+    NewArticle {
+        headline: String::from("aefafasf"),
+        location: String::from("aefafasf"),
+        author:String::from("aefafasf"),
+        content: String::from("aefafasf")
+    }
+}
+
+//最后一个，让任何实现了Display的类型调用ToString
+//  impl<T: fmt::Display> ToString for T {}
+
+
+
+
+
+
+
 pub struct NewArticle {
     pub headline: String,
     pub location: String,
